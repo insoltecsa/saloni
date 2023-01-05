177942a513e5b104d8719646eec166483321594c
Byron Moreira <89430643+insoltecsa@users.noreply.github.com>
2023-01-05 15:59:07 -0600

    Carga de modulos v16

---
+++ fix_percentage_discount_for_user_app/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name" : "Fix and Percentage Discount Limit For User",
+    "author": "Edge Technologies",
+    "version" : "14.0.1.3",
+    "live_test_url":'https://youtu.be/m6pwEipqiYw',
+    "images":["static/description/main_screenshot.png"],
+    'summary': 'User Fix Discount Limit For user set limit of discount to users fix discount fixed discount extension limit discount product limited discount restrict discount Sale Discount Limit Restriction Customer discount limit fixed user discount Restriction limit',
+    "description": """ Fix & Percentage Discount Limit For User helps to set limit of discount to users. 
+
+
+
+Percentage Discount fix discount fixed discount 
+discount limit 
+limit discount 
+limit product discount 
+limited discount 
+restrict discount 
+discount limit 
+Discount extension limits
+pos Discount limits
+Sale Discount Limit
+Restriction Customer discount limit discount Restriction limit
+Customer discount limit
+fixed user discount
+discount Restriction limit 
+
+    """,
+    "license" : "OPL-1",
+    "depends" : ['base','sale_management','account'],
+    "data": [
+        'views/users_inherit_views.xml',
+    ],
+    'qweb': [
+    ],
+    "auto_install": False,
+    "installable": True,
+    "price": 5,
+    "currency": 'EUR',
+    "category" : "Sales",
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
